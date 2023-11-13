2feb9fe845c30be3fcc84587230c8537952e25a9
Abd-UlRahman <a.khalid5322@gmail.com>
2023-11-13 12:03:28 +0200

    Adding Form

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
+import 'package:shopping_list_app/model/categories.dart';
 
 class NewItem extends StatefulWidget{
   NewItem({super.key});
@@ -17,7 +19,49 @@ class _StateNewItem extends State<NewItem>{
     return Scaffold(
       appBar: AppBar(actions: [],
       title: Text('Add New Item'),),
-      body: Text('New Item'),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(child: Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              decoration: const InputDecoration(
+                label: Text('Name'),
+              ),
+              validator: (vale){return 'Demo...';},
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    initialValue: '1',
+                    decoration: const InputDecoration(
+                      label: Text('Quantity'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: DropdownButtonFormField(items: [
+                    for (final category in categories.entries)//entries change the map into List
+                    DropdownMenuItem(
+                      value: category.value,
+                      child: Row(
+                      children: [
+                        Container(
+                          width: 6,
+                          height: 6,
+                          color: category.value.color,
+                        ),
+                        SizedBox(width: 6,),
+                        Text(category.value.title)
+                      ],
+                    ))
+                  ], onChanged: (value){}),
+                )
+              ],
+            )
+          ],
+        )),),
     );
   }
 }
